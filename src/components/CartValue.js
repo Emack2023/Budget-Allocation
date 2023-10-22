6d8295b5bc9bab9c120c28b40805d95e7fa5afce
Emack2023 <147569908+Emack2023@users.noreply.github.com>
2023-10-22 19:17:15 -0400

    Update CartValue.js

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -9,7 +9,7 @@ const CartValue = () => {
 
     return (
         <div className='alert alert-primary'>
-            <span>Cart Value: {Location}{totalExpenses}</span>
+            <span>Spent So Far: {Location}{totalExpenses}</span>
         </div>
     );
 };
